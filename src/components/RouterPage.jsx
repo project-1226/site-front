9944983f8597e2fb1074c10c2a6ef11d1e600f6d
Seonu1015 <ulxovoxlu1015@gmail.com>
2024-01-01 02:26:49 +0900

    Feat: 로그인 UI 구성 및 primary color theme 추가

--- src/components/RouterPage.jsx
+++ src/components/RouterPage.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./HomePage";
+import SigninPage from "./user/SigninPage";
 
 const RouterPage = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+
+      {/* user */}
+      <Route path="/login" element={<SigninPage />} />
+    </Routes>
+  );
 };
 
 export default RouterPage;
--- src/components/user/GoogleLogin.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const GoogleLogin = () => {
-    return <div>GoogleLogin</div>;
-};
-
-export default GoogleLogin;
