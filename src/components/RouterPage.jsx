16d8607125bcc0d64d59c4b391473e540a9f2091
AK47 <ayanamirei2@naver.com>
2024-01-02 09:54:34 +0900

    add AI, CHATBOT

--- src/components/RouterPage.jsx
+++ src/components/RouterPage.jsx
@@ -4,6 +4,8 @@ import HomePage from "./HomePage";
 
 import Imagereader from "./ai/Imagereader";
 import MotionReader from "./ai/MotionReader";
+import Chatbot from "./chatbot/Chatbot";
+import AiList from "./ai/AiList";
 
 const RouterPage = () => {
     return (
@@ -11,6 +13,8 @@ const RouterPage = () => {
             <Route path="/" element={<HomePage />} />
             <Route path="/AIimg" element={<Imagereader />} />
             <Route path="/AImotion" element={<MotionReader />} />
+            <Route path="/Chatbot" element={<Chatbot />} />
+            <Route path="/Ailist" element={<AiList />} />
         </Routes>
     );
 };
--- src/components/ai/MotionReader.jsx
+++ src/components/ai/MotionReader.jsx
@@ -1,68 +1,119 @@
-import React, { useState, useEffect, useRef } from "react";
-import WebCam from "react-webcam";
-import { CircularProgress } from "@mui/material";
+import React, { useRef, useEffect,useState } from 'react';
+import Webcam from 'react-webcam';
+import * as tmPose from '@teachablemachine/pose';
 
+const modelURL = 'https://teachablemachine.withgoogle.com/models/jwqc-NFPH/';
+let model, webcam, ctx, labelContainer, maxPredictions;
 
-//Our tensorflow library
-let ml5;
+const MotionReader = () => {
+  const webcamRef = useRef(null);
+  const modelRef = useRef(null);
+  const [moveresult,setmoveResult]=useState(null);
+  let [test, setTest]=useState(0);
+  let [moveCnt, setMoveCnt]=useState(0);
+	let movecounter = 0;
+  useEffect(() => {
+    const init = async () => {
+      const modelURL = 'https://teachablemachine.withgoogle.com/models/jwqc-NFPH/model.json';
+      const metadataURL = 'https://teachablemachine.withgoogle.com/models/jwqc-NFPH/metadata.json';
 
-//Teachable machine url after uploading your model
-const modelURL = "https://teachablemachine.withgoogle.com/models/S0rqlc69D/";
-let classifier;
+      // Load the model and metadata
+      model = await tmPose.load(modelURL, metadataURL);
+      maxPredictions = model.getTotalClasses();
+      modelRef.current = model;
 
-export default function MotionReader() {
-	const [loading, setLoading] = useState(true);
+      // Convenience function to set up a webcam
+      const size = 500;
+      const flip = true; // Whether to flip the webcam
+      webcam = new tmPose.Webcam(size, size, flip); // Width, height, flip
+      await webcam.setup(); // Request access to the webcam
+      await webcam.play();
+      window.requestAnimationFrame(loop);
 
-	//React hook for accessing the DOM element
-	const webcamRef = useRef(null);
+      // Append/get elements to the DOM
+      const canvas = document.getElementById('canvas');
+      canvas.width = size;
+      canvas.height = size;
+      ctx = canvas.getContext('2d');
+      labelContainer = document.getElementById('label-container');
+      for (let i = 0; i < maxPredictions; i++) {
+        // And class labels
+        labelContainer.appendChild(document.createElement('div'));
+      }
+    };
 
-	//Label to be predicted by our classifier
-	const [label, setLabel] = useState("");
-
-	const videoConstraints = {
-		width: 1280,
-		height: 720,
-		facingMode: "user",
-	};
-
-	//We're using asynchronous useEffect so we can wait for our classifier to be initialized before we attempt to use it
-	useEffect(() => {
-		ml5 = require("ml5"); //Workaround because of "window is not defined" error
-		(async () => {
-			classifier = await ml5.imageClassifier(modelURL + "model.json");
-			classifyVideo();
-		})();
-	}, []);
-
-	const classifyVideo = () => {
-		try {
-			loading && setLoading(false);
-			//Get the classifications and pass it to callback function
-			classifier.classify(webcamRef.current.video, gotResults);
-		} catch (err) {
-			console.log(err.message);
-		}
-	};
-
-	const gotResults = async (error, results) => {
-		const label = results[0].label; //Predicted label
-		setLabel(label);
-		classifyVideo(); // Run on next webcam image
-	};
-
-	return (
-		<div >
-			
-					<WebCam
-						audio={false}
-						height={480}
-						ref={webcamRef}
-						screenshotFormat="image/jpeg"
-						width={720}
-						videoConstraints={videoConstraints}
-					/>
-		
+    init();
+  }, []);
+  useEffect(() => {
+    if (test === '1.00') {
+		setMoveCnt(prevMoveCnt => prevMoveCnt + 1);
+		console.log(moveCnt);
+	  }
 	
-		</div>
-	);
-}
+  },[test]);
+
+  const loop = async (timestamp) => {
+    webcam.update(); // Update the webcam frame
+    await predict();
+    window.requestAnimationFrame(loop);
+  };
+
+  const predict = async () => {
+    // Prediction #1: run input through posenet
+    const { pose, posenetOutput } = await modelRef.current.estimatePose(webcam.canvas);
+    // Prediction #2: run input through the Teachable Machine classification model
+    const prediction = await modelRef.current.predict(posenetOutput);
+
+	
+    for (let i = 0; i < maxPredictions; i++) {
+      const classPrediction =
+        prediction[i].className + ': ' + prediction[i].probability.toFixed(2);
+		//console.log( prediction[1].probability);
+		setTest( prediction[0].probability.toFixed(2));
+        
+		labelContainer.childNodes[i].innerHTML = classPrediction;
+	  //console.log(prediction[1].probability);
+	
+		
+		
+			// if(prediction[0].probability.toFixed(2)==1.00){
+            //     movecounter++;
+			// }
+			//console.log(movecounter)
+
+	 
+
+
+    }
+
+    // Finally, draw the poses
+    drawPose(pose);
+  };
+  
+
+
+  
+  const drawPose = (pose) => {
+    if (webcam.canvas) {
+      ctx.drawImage(webcam.canvas, 0, 0);
+      // Draw the keypoints and skeleton
+      if (pose) {
+        const minPartConfidence = 0.5;
+        tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx);
+        tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
+      }
+    }
+  };
+
+  return (
+    <div>
+		<h1>동작인식</h1>
+		<hr></hr>
+      
+      <canvas id="canvas"></canvas>
+      <div id="label-container"></div>
+    </div>
+  );
+};
+
+export default MotionReader;
