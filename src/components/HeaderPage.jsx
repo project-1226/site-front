715a081abe9f54e1cbc707d78bd94e0114c67cae
Seonu1015 <ulxovoxlu1015@gmail.com>
2024-01-03 00:36:10 +0900

    Feat: react-hook-form을 이용한 회원가입, 로그인의 유효성 검사 추가

--- src/components/HeaderPage.jsx
+++ src/components/HeaderPage.jsx
@@ -1,40 +1,86 @@
-import { Menu } from "@mui/icons-material";
+import { SetMeal } from "@mui/icons-material";
 import {
-    AppBar,
-    Box,
-    Button,
-    IconButton,
-    Toolbar,
-    Typography,
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Stack,
+  Toolbar,
+  Typography,
 } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HeaderPage = () => {
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <Menu />
-                    </IconButton>
-                    <Typography
-                        variant="h6"
-                        component="div"
-                        sx={{ flexGrow: 1 }}
-                    >
-                        Home
-                    </Typography>
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
-            </AppBar>
-        </Box>
-    );
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            component={Link}
+            to="/"
+            sx={{ mr: 2 }}
+          >
+            <SetMeal />
+          </IconButton>
+          <Stack
+            direction="row"
+            spacing={4}
+            sx={{ flexGrow: 1 }}
+            justifyContent="center"
+          >
+            <Typography
+              color="inherit"
+              component={Link}
+              to="/"
+              sx={{ textDecoration: "none" }}
+            >
+              내 식단
+            </Typography>
+            <Typography
+              color="inherit"
+              component={Link}
+              to="/"
+              sx={{ textDecoration: "none" }}
+            >
+              건강식단
+            </Typography>
+            <Typography
+              color="inherit"
+              component={Link}
+              to="/"
+              sx={{ textDecoration: "none" }}
+            >
+              질환맞춤식단
+            </Typography>
+            <Typography
+              color="inherit"
+              component={Link}
+              to="/"
+              sx={{ textDecoration: "none" }}
+            >
+              헬스케어
+            </Typography>
+            <Typography
+              color="inherit"
+              component={Link}
+              to="/"
+              sx={{ textDecoration: "none" }}
+            >
+              커뮤니티
+            </Typography>
+          </Stack>
+          <Button color="inherit" component={Link} to="/login">
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 };
 
 export default HeaderPage;
--- src/components/user/SignupPage.jsx
+++ src/components/user/SignupPage.jsx
@@ -1,183 +1,155 @@
-import { LockOutlined, Visibility, VisibilityOff } from "@mui/icons-material";
+import { LockOutlined } from "@mui/icons-material";
 import {
-    Alert,
-    Avatar,
-    Box,
-    Button,
-    Checkbox,
-    Container,
-    CssBaseline,
-    FormControlLabel,
-    Grid,
-    IconButton,
-    InputAdornment,
-    Link,
-    TextField,
-    Typography,
+  Alert,
+  Avatar,
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  Grid,
+  Link,
+  TextField,
+  Typography,
 } from "@mui/material";
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useRef } from "react";
+import { useForm } from "react-hook-form";
 
 const SignupPage = () => {
-    const [isEmailNull, setIsEmailNull] = useState(false);
-    const [isEmail, setIsEmail] = useState(false);
-    const [isPassNull, setIsPassNull] = useState(false);
-    const [equalPass, setEqualPass] = useState(0);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch, // 비밀번호 일치 여부 확인을 위해 사용
+  } = useForm();
 
-    const [showPassword, setShowPassword] = React.useState(false);
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
-    const handleMouseDownPassword = (event) => {
-        event.preventDefault();
-    };
+  const password = useRef({});
+  password.current = watch("password", "");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const data = new FormData(e.currentTarget);
+  const onSubmit = async (data) => {
+    console.log(data);
+    if (window.confirm("회원 가입을 진행하시겠습니까?")) {
+      await axios
+        .post("/user/insert", data)
+        .then((response) => {
+          alert(
+            "회원가입을 환영합니다!\n가입 기념으로 3000포인트를 지급해드렸습니다!"
+          );
+          window.location.href = "/";
+        })
+        .catch((error) => {
+          alert("이미 가입된 이메일입니다.");
+        });
+    }
+  };
 
-        if (data.get("email") === "") {
-            setIsEmailNull(true);
-        } else if (!data.get("email").includes("@")) {
-            setIsEmailNull(false);
-            setIsEmail(true);
-        } else {
-            setIsEmail(false);
-        }
-
-        if (data.get("password") === "") {
-            setIsPassNull(true);
-        } else {
-            setIsPassNull(false);
-        }
-
-        if (isEmail && !(isEmailNull || isPassNull)) {
-            if (window.confirm("회원 가입을 진행하시겠습니까?")) {
-                await axios.post("/user/insert", data);
-                alert("회원가입 완료!");
-            }
-        }
-    };
-
-    return (
-        <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <Box
-                sx={{
-                    marginTop: 8,
-                    marginBottom: 8,
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                }}
-            >
-                <Avatar sx={{ m: 1, bgcolor: "grey" }}>
-                    <LockOutlined />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Sign up
-                </Typography>
-                <Box
-                    component="form"
-                    noValidate
-                    onSubmit={handleSubmit}
-                    sx={{ mt: 3 }}
-                >
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <TextField
-                                required
-                                fullWidth
-                                id="email"
-                                label="이메일"
-                                name="email"
-                                autoComplete="email"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                required
-                                fullWidth
-                                name="password1"
-                                label="비밀번호"
-                                type={showPassword ? "text" : "password"}
-                                id="password1"
-                                autoComplete="new-password"
-                                endAdornment={
-                                    <InputAdornment position="end">
-                                        <IconButton
-                                            aria-label="toggle password visibility"
-                                            onClick={handleClickShowPassword}
-                                            onMouseDown={
-                                                handleMouseDownPassword
-                                            }
-                                            edge="end"
-                                        >
-                                            {showPassword ? (
-                                                <VisibilityOff />
-                                            ) : (
-                                                <Visibility />
-                                            )}
-                                        </IconButton>
-                                    </InputAdornment>
-                                }
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                required
-                                fullWidth
-                                name="password2"
-                                label="비밀번호 확인"
-                                type="password"
-                                id="password2"
-                                autoComplete="new-password"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <FormControlLabel
-                                control={
-                                    <Checkbox
-                                        value="allowExtraEmails"
-                                        color="primary"
-                                    />
-                                }
-                                label="I want to receive inspiration, marketing promotions and updates via email."
-                            />
-                        </Grid>
-                    </Grid>
-                    {isEmailNull && (
-                        <Alert severity="error" sx={{ mt: 2 }}>
-                            이메일을 입력해 주세요.
-                        </Alert>
-                    )}
-                    {isEmail && (
-                        <Alert severity="error" sx={{ mt: 2 }}>
-                            이메일 형식이 아닙니다.
-                        </Alert>
-                    )}
-                    {isPassNull && (
-                        <Alert severity="error" sx={{ mt: 2 }}>
-                            비밀번호를 입력해 주세요.
-                        </Alert>
-                    )}
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{ mt: 3, mb: 2 }}
-                    >
-                        Sign Up
-                    </Button>
-                    <Grid container justifyContent="flex-end">
-                        <Grid item>
-                            <Link href="#" variant="body2">
-                                Already have an account? Sign in
-                            </Link>
-                        </Grid>
-                    </Grid>
-                </Box>
-            </Box>
-        </Container>
-    );
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          marginBottom: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "grey" }}>
+          <LockOutlined />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <Box
+          component="form"
+          noValidate
+          onSubmit={handleSubmit(onSubmit)}
+          sx={{ mt: 3 }}
+        >
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                {...register("email", {
+                  required: true,
+                  pattern: /^\S+@\S+$/i,
+                })}
+                required
+                fullWidth
+                id="email"
+                label="이메일"
+                name="email"
+                autoComplete="email"
+              />
+              {errors.email && errors.email.type === "required" && (
+                <Alert severity="error" sx={{ mt: 2 }}>
+                  이메일을 입력해 주세요.
+                </Alert>
+              )}
+              {errors.email && errors.email.type === "pattern" && (
+                <Alert severity="error" sx={{ mt: 2 }}>
+                  이메일 형식이 아닙니다.
+                </Alert>
+              )}
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                {...register("password", { required: true })}
+                required
+                fullWidth
+                name="password"
+                label="비밀번호 확인"
+                type="password"
+                id="password"
+                autoComplete="new-password"
+              />
+              {errors.password1 && (
+                <Alert severity="error" sx={{ mt: 2 }}>
+                  비밀번호를 입력해 주세요.
+                </Alert>
+              )}
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                {...register("passwordCheck", {
+                  validate: (value) =>
+                    value === password.current ||
+                    "비밀번호가 일치하지 않습니다.",
+                })}
+                required
+                fullWidth
+                name="passwordCheck"
+                label="비밀번호 확인"
+                type="password"
+                id="passwordCheck"
+                autoComplete="new-password"
+              />
+              {errors.password2 && (
+                <Alert severity="error" sx={{ mt: 2 }}>
+                  {errors.password2.message}
+                </Alert>
+              )}
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign Up
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="/login" variant="body2">
+                이미 계정이 있으신가요?
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
 };
 
 export default SignupPage;
