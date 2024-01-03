b161b4b82a16eb27ca8a25447c89082512ce5723
sori-k <vrti0913@naver.com>
2024-01-03 17:02:41 +0900

    Feat:커뮤니티 페이지 UI 구성

--- src/components/RouterPage.jsx
+++ src/components/RouterPage.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./HomePage";
 import SigninPage from "./user/SigninPage";
+import SideMenu from "./community/SideMenu";
+import NoticePage from "./community/NoticePage";
+import ReviewPage from "./community/ReviewPage";
 
 const RouterPage = () => {
   return (
@@ -10,6 +13,11 @@ const RouterPage = () => {
 
       {/* user */}
       <Route path="/login" element={<SigninPage />} />
+
+      {/* community */}
+      <Route path="/community" element={<SideMenu/>} />
+      <Route path="/community/notice" element={<NoticePage/>} />
+      <Route path="/community/review" element={<ReviewPage/>} />
     </Routes>
   );
 };
