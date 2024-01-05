95e5459da965fad37f3d683f426e684b1acba09b
Seonu1015 <ulxovoxlu1015@gmail.com>
2024-01-05 18:45:59 +0900

    WIP: 배송지 관리 페이지 구현 중

---
+++ src/components/user/mypage/info/AddressList.jsx
@@ -0,0 +1,15 @@
+import { Card, Typography } from "@mui/material";
+import React from "react";
+
+const AddressList = () => {
+  return (
+    <>
+      <Typography variant="h6" gutterBottom sx={{ fontWeight: "bolder" }}>
+        배송지 관리
+      </Typography>
+      <Card></Card>
+    </>
+  );
+};
+
+export default AddressList;
