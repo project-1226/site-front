7e9487e25856542d10b3221f3ab94403bc7d8189
Seonu1015 <ulxovoxlu1015@gmail.com>
2024-01-10 06:31:44 +0900

    WIP: 리뷰 수정 기능 추가중

---
+++ src/components/user/mypage/activity/UpdateReview.jsx
@@ -0,0 +1,265 @@
+import { Star } from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Paper,
+  Rating,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useEffect, useRef, useState } from "react";
+import Draggable from "react-draggable";
+import ImageUploader from "../shop/ImageUploader";
+import axios from "axios";
+
+function PaperComponent(props) {
+  return (
+    <Draggable
+      handle="#draggable-dialog-title"
+      cancel={'[class*="MuiDialogContent-root"]'}
+    >
+      <Paper {...props} />
+    </Draggable>
+  );
+}
+
+const labels = {
+  1: "나쁨",
+  2: "별로",
+  3: "보통",
+  4: "좋음",
+  5: "최고",
+};
+
+function getLabelText(value) {
+  return `${value} Star${value !== 1 ? "s" : ""}, ${labels[value]}`;
+}
+
+const UpdateReview = ({ product_reviewid }) => {
+  const [loading, setLoading] = useState(false);
+  const [score, setScore] = useState(0);
+  const [hover, setHover] = useState(-1);
+  const [open, setOpen] = useState(false);
+
+  const [content, setContent] = useState("");
+  let form = {
+    userid: sessionStorage.getItem("userid"),
+    productid: 1,
+    score,
+    content,
+    image_names: "",
+    image_urls: "",
+  };
+
+  const uploaderRef = useRef(null);
+
+  const getReview = async () => {
+    try {
+      const res = await axios("/product_review/read", {
+        params: {
+          product_reviewid,
+        },
+      });
+    } catch (error) {
+      console.error("Error - read review:", error);
+    }
+  };
+
+  const onSubmit = async () => {
+    setLoading(true);
+
+    try {
+      const [imageNames, uploadedURLs] = await uploaderRef.current.onUpload();
+      // console.log(uploadedURLs);
+      if (uploadedURLs) {
+        form = {
+          userid: sessionStorage.getItem("userid"),
+          productid: 2,
+          score,
+          content,
+          image_names: imageNames.join(","),
+          image_urls: uploadedURLs.join(","),
+        };
+        // console.log(form);
+      }
+      //   await axios.post("/product_review/insert", form);
+      setLoading(false);
+      alert("리뷰가 등록되었습니다.");
+      window.location.href = "/mp/mprch";
+    } catch (error) {
+      setLoading(false);
+      console.log("insert review:", error);
+      alert("리뷰 등록이 실패하였습니다.\n관리자에게 문의해주세요.");
+    }
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    getReview();
+  }, [open]);
+
+  return (
+    <>
+      <Button variant="contained" size="small" onClick={handleClickOpen}>
+        수정
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperComponent={PaperComponent}
+        aria-labelledby="draggable-dialog-title"
+        fullWidth
+        maxWidth="md"
+      >
+        <DialogTitle component="h6" sx={{ fontWeight: "bolder", ml: 1.7 }}>
+          리뷰 수정
+        </DialogTitle>
+        <DialogContent>
+          <TableContainer>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableBody>
+                <TableRow
+                  sx={{
+                    "&:last-child td, &:last-child th": {
+                      border: 0,
+                    },
+                  }}
+                >
+                  <TableCell width={150}>
+                    <img
+                      src="http://via.placeholder.com/500x500"
+                      alt="product"
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <Stack spacing={1}>
+                      <Typography variant="h6" sx={{ fontWeight: "bolder" }}>
+                        상품명
+                      </Typography>
+                      <Stack direction="row" spacing={2} alignItems="center">
+                        <Rating
+                          name="hover-feedback"
+                          value={score}
+                          precision={1}
+                          size="large"
+                          getLabelText={getLabelText}
+                          onChange={(event, newValue) => {
+                            setScore(newValue);
+                          }}
+                          onChangeActive={(event, newHover) => {
+                            setHover(newHover);
+                          }}
+                          emptyIcon={
+                            <Star
+                              style={{ opacity: 0.55 }}
+                              fontSize="inherit"
+                            />
+                          }
+                        />
+                        {score !== null && (
+                          <Box sx={{ ml: 2 }}>
+                            {labels[hover !== -1 ? hover : score]}
+                          </Box>
+                        )}
+                      </Stack>
+                    </Stack>
+                  </TableCell>
+                </TableRow>
+                <TableRow
+                  sx={{
+                    "&:last-child td, &:last-child th": {
+                      border: 0,
+                    },
+                  }}
+                >
+                  <TableCell width={150} sx={{ verticalAlign: "top" }}>
+                    <Typography
+                      variant="body1"
+                      sx={{
+                        fontWeight: "bolder",
+                        mt: 0.5,
+                        ml: 0.5,
+                      }}
+                    >
+                      상세 리뷰
+                    </Typography>
+                  </TableCell>
+                  <TableCell>
+                    <TextField
+                      id="outlined-multiline-static"
+                      fullWidth
+                      multiline
+                      rows={4}
+                      placeholder="다른 고객님에게 도움이 되도록 상품에 대한 솔직한 평가를 남겨주세요."
+                      value={content}
+                      onChange={(e) => setContent(e.target.value)}
+                      sx={{ mt: 0.5, mb: 2 }}
+                    />
+                    <Typography variant="caption" color="grey">
+                      상품 품질과 관계없는 내용은 비공개 처리될 수 있습니다.
+                      <br />
+                      작성된 리뷰는 삭제 전까지 '상품 리뷰'에 공개되고, 'My 활동
+                      {" > "}
+                      활동 내역 {">"} 리뷰'에서 수정 및 삭제가 가능합니다.
+                    </Typography>
+                  </TableCell>
+                </TableRow>
+                <TableRow
+                  sx={{
+                    "&:last-child td, &:last-child th": {
+                      border: 0,
+                    },
+                  }}
+                >
+                  <TableCell width={150} sx={{ verticalAlign: "top" }}>
+                    <Typography
+                      variant="body1"
+                      sx={{
+                        fontWeight: "bolder",
+                        mt: 0.5,
+                        ml: 0.5,
+                      }}
+                    >
+                      사진 첨부
+                    </Typography>
+                  </TableCell>
+                  <TableCell>
+                    <ImageUploader ref={uploaderRef} />
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </DialogContent>
+        <DialogActions sx={{ mr: 2, mb: 2 }}>
+          <Button variant="contained" onClick={onSubmit}>
+            수정
+          </Button>
+          <Button variant="outlined" autoFocus onClick={handleClose}>
+            취소
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+
+export default UpdateReview;
