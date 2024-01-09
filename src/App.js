7a92551e4213f0623bbba892462e696761adb478
Seonu1015 <ulxovoxlu1015@gmail.com>
2024-01-09 17:03:15 +0900

    Fix: 라우터 해결

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import HeaderPage from "./components/HeaderPage";
 import RouterPage from "./components/RouterPage";
 import FooterPage from "./components/FooterPage";
@@ -6,34 +6,17 @@ import SurveyPage from "./components/SurveyPage";
 import QuestionsPage from "./components/QuestionsPage";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 function App() {
-  const userid = sessionStorage.getItem('userid');
+  const userid = sessionStorage.getItem("userid");
   const navigate = useNavigate();
   const [showSurvey, setShowSurvey] = useState(false);
-
-  const handleSurveyBtnClick = () => {
-    setShowSurvey(true);
-    navigate('/survey/questions');
-  };
-
-  
-
-  const handleBackButtonClick = () => {
-    navigate('/survey');
-  };
 
   return (
     <ThemeProvider theme={theme}>
       {!showSurvey && <HeaderPage />}
-      {!showSurvey ? (
-        <SurveyPage onQuestionsBtnClick={handleSurveyBtnClick} />
-      ) : (
-        <>
-          {userid ? <RouterPage /> : <QuestionsPage setShowSurvey={setShowSurvey}/>}
-        </>
-      )}
+      <RouterPage />
       {!showSurvey && <FooterPage />}
     </ThemeProvider>
   );
