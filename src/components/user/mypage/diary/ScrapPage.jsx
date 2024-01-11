baf677469bb33f8ef3bef52e9b32fc07f0f08ef6
Seonu1015 <ulxovoxlu1015@gmail.com>
2024-01-11 18:24:25 +0900

    WIP: 스크랩 페이지 작업 중

--- src/components/user/mypage/diary/ScrapPage.jsx
+++ src/components/user/mypage/diary/ScrapPage.jsx
@@ -1,7 +1,36 @@
+import { TabContext, TabList, TabPanel } from "@mui/lab";
+import { Box, Tab } from "@mui/material";
 import React from "react";
 
 const ScrapPage = () => {
-  return <div>ScrapPage</div>;
+  const [value, setValue] = React.useState("1");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        typography: "body1",
+        bgcolor: "transparent",
+        py: 4.5,
+        pr: 3,
+      }}
+    >
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label="식단" value="1" />
+            <Tab label="운동" value="2" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">식단 스크랩</TabPanel>
+        <TabPanel value="2">운동 스크랩</TabPanel>
+      </TabContext>
+    </Box>
+  );
 };
 
 export default ScrapPage;
