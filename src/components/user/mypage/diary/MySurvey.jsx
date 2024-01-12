c488fd3db3e15d2c7f492f963c752f28b05212b2
Seonu1015 <ulxovoxlu1015@gmail.com>
2024-01-12 18:33:46 +0900

    WIP: 설문조사 결과 페이지 UI 구성 중

--- src/components/user/mypage/diary/MySurvey.jsx
+++ src/components/user/mypage/diary/MySurvey.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
+import { Box, Tab } from "@mui/material";
+import React, { useState } from "react";
+import UpdateFood from "./UpdateFood";
+import UpdateExcercise from "./UpdateExcercise";
+import UpdateFit from "./UpdateFit";
 
 const MySurvey = () => {
-    return <div>MySurvey</div>;
+  // 식단정보, 운동정보, 신체정보 로 나눠서 변경할 수 있게 하고 첫 페이지에는 그 결과를 보여주는걸로!
+
+  const [value, setValue] = useState("1");
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <Box sx={{ width: "100%", bgcolor: "transparent", py: 5, pr: 3 }}>
+      <TabContext value={value}>
+        <Box>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label="설문결과" value="1" />
+            <Tab label="신체정보" value="2" />
+            <Tab label="식단정보" value="3" />
+            <Tab label="운동정보" value="4" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">설문결과</TabPanel>
+        <TabPanel value="2">
+          <UpdateFit />
+        </TabPanel>
+        <TabPanel value="3">
+          <UpdateFood />
+        </TabPanel>
+        <TabPanel value="4">
+          <UpdateExcercise />
+        </TabPanel>
+      </TabContext>
+    </Box>
+  );
 };
 
 export default MySurvey;
---
+++ src/components/user/mypage/diary/UpdateExcercise.jsx
@@ -0,0 +1,51 @@
+import {
+  Table,
+  Paper,
+  Stack,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Typography,
+  Button,
+} from "@mui/material";
+import React from "react";
+
+const UpdateExcercise = () => {
+  return (
+    <>
+      <TableContainer component={Paper} sx={{ mt: 3 }}>
+        <Table sx={{ minWidth: 800 }} aria-label="simple table">
+          <TableBody>
+            <TableRow>
+              <TableCell
+                component="th"
+                sx={{
+                  width: "20%",
+                  color: "gray",
+                  fontWeight: "bold",
+                  py: 3.2,
+                }}
+              >
+                목표
+              </TableCell>
+              <TableCell>
+                <Stack direction="row" alignItems="center" spacing={3}>
+                  <Typography>목표</Typography>
+                </Stack>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Stack direction="row" sx={{ my: 2 }} spacing={2} justifyContent="center">
+        <Button variant="contained">수정하기</Button>
+        <Button variant="contained" color="secondary">
+          돌아가기
+        </Button>
+      </Stack>
+    </>
+  );
+};
+
+export default UpdateExcercise;
