de163651d619a3ef91439e140adc937269d27bcd
Seonu1015 <ulxovoxlu1015@gmail.com>
2024-01-15 00:28:36 +0900

    WIP: 다이어리 - 플랜 UI 구성 중

--- src/components/user/mypage/diary/MyExcercise.jsx
+++ src/components/user/mypage/diary/MyExcercise.jsx
@@ -1,4 +1,12 @@
-import { Backdrop, CircularProgress } from "@mui/material";
+import { ExpandMore } from "@mui/icons-material";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Backdrop,
+  CircularProgress,
+  Typography,
+} from "@mui/material";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
@@ -11,8 +19,23 @@ const MyExcercise = () => {
     const res = await axios("/user/my-excercise", {
       params: { userid: sessionStorage.getItem("userid") },
     });
-    console.log(res.data);
-    setList(res.data);
+    // console.log(res.data);
+
+    const data = res.data.reduce((acc, item) => {
+      if (!acc[item.categoryid]) {
+        acc[item.categoryid] = {
+          categoryid: item.categoryid,
+          categoryname: item.categoryname,
+          excercises: [],
+        };
+      }
+      acc[item.categoryid].excercises.push(item.excercisename);
+      return acc;
+    }, {});
+
+    const dataArray = Object.values(data);
+    setList(dataArray);
+    console.log(list);
     setLoading(false);
   };
 
@@ -28,7 +51,23 @@ const MyExcercise = () => {
       >
         <CircularProgress color="inherit" />
       </Backdrop>
-      {list.map((l) => l.categoryname)}
+      <div>
+        <Typography sx={{ pb: 2 }}>OO님을 위한 추천 운동</Typography>
+        {list.map((l, index) => (
+          <Accordion key={index} sx={{ width: "60%" }}>
+            <AccordionSummary
+              expandIcon={<ExpandMore />}
+              aria-controls="panel1-content"
+              id="panel1-header"
+            >
+              {l.categoryname} 운동
+            </AccordionSummary>
+            {l.excercises.map((e) => (
+              <AccordionDetails sx={{ ml: 5 }}>{e}</AccordionDetails>
+            ))}
+          </Accordion>
+        ))}
+      </div>
     </>
   );
 };
