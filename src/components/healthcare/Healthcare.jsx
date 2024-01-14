c64f37464e1cf92819ccb6e1b4e910d422c7ea74
SongAhreum <thddkfmathd@naver.com>
2024-01-14 22:03:41 +0900

    WIP: 운동페이지 구현중

--- src/components/healthcare/Healthcare.jsx
+++ src/components/healthcare/Healthcare.jsx
@@ -2,6 +2,8 @@
 import React, { useState,useEffect } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import HealthcareModal from './HealthcareModal';
+import HealthcareModal2 from './HealthcareModal2';
+import { DataGrid } from '@mui/x-data-grid';
 import {
   Backdrop,
   Button,
@@ -11,7 +13,11 @@ import {
   CardActions,
   CircularProgress,
   Typography,
+  Tabs,
+  Tab, 
+  Box
 } from "@mui/material";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
 import IconButton, { IconButtonProps } from '@mui/material/IconButton';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
@@ -21,9 +27,51 @@ import axios from 'axios';
 
 const Healthcare = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen2, setIsModalOpen2] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState('');
   const [categories,setCategories] = useState([]);
-  
+  const [myExercise,setMyExercise] = useState([]);
+  const columns = [
+    { field: 'excerciseid', headerName: 'Exercise ID', width: 120 },
+    { field: 'name', headerName: 'Name', width: 200 },
+    {
+      field: 'description',
+      headerName: 'Description',
+      width: 400,
+    },{
+      field: 'action1',
+      headerName: '',
+      width: 150,
+      renderCell: (params) => (
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={(e) => isModalOpen2(true)}
+        >
+          운동영상
+        </Button>
+      ),
+    },
+    {
+      field: 'action2',
+      headerName: 'AI',
+      width: 150,
+      renderCell: (params) => (
+        params.row.ai == 1 &&
+        <Button
+          variant="contained"
+          color="primary" 
+          onClick={(e) => isModalOpen2(true)}       
+        >
+          AI와 운동하기
+        </Button>
+      ),
+    },
+  ];
+  const [value, setValue] = React.useState('one');
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   
   const getCategories = async () => {
     try {
@@ -39,7 +87,11 @@ const Healthcare = () => {
       //카테고리들고와서 하는 내용
     }
   };
-  
+  const getMyExercise = async() =>{
+    const res = await axios(`/exercise/myexercises?userid=${sessionStorage.getItem("userid")}&categoryid=${value}`)
+    console.log(res.data)
+    setMyExercise(res.data);
+  }
   useEffect(() => {
     getCategories();
   }, [])
@@ -50,9 +102,27 @@ const Healthcare = () => {
     setIsModalOpen(true);
   };
 
-  const handleCloseModal = () => {
+  const handleCloseModal = () => { 
     setIsModalOpen(false);
   };
+  const handleCloseModal2 = () => { 
+    setIsModalOpen2(false);
+  };
+  useEffect(()=>{
+    getMyExercise();
+  },[value])
+
+  //운동영상보러가기 클릭 
+  const handleYoutubeClick = (event,row) => {
+    event.stopPropagation();    
+    console.log('Button clicked for row:', row);
+  };
+  //AI와 운동하기 선택
+  const handleAIClick =(event,row) =>{
+    event.stopPropagation();   
+    console.log('ai!!!!!!!!Button clicked for row:', row);
+  }
+
 
   return (
     <div className='healthcare_wrap'>
@@ -68,7 +138,46 @@ const Healthcare = () => {
         )}      
       </div>
 
-
+    {/* 내가선택한 운동 리스트  */}
+    
+   <Box
+      sx={{
+        width: "100%",
+        typography: "body1",
+        bgcolor: "transparent",
+        py: 4.5,
+        pr: 3,
+      }}
+    >
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+          {categories.map((category=>
+          <Tab value={category.categoryid} label={category.name} />                   
+          ))}
+          </TabList>
+        </Box>
+        <TabPanel value={value}>
+        
+        {myExercise.length == 0 ?
+          <div><h1>해당 카테고리에서 선택한운동이 없습니다</h1></div>
+        :
+        <DataGrid
+          rows={myExercise}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: { page: 0, pageSize: 5 },
+            },
+          }}  // 한 페이지당 보이는 행 수 설정
+          pageSizeOptions={[5, 10]}
+          getRowId={(row) => row.excerciseid}         
+        />
+        }
+          
+        </TabPanel>     
+      </TabContext>
+    </Box>
 
       <div className='healthcare_contents'>
         <section>
@@ -223,6 +332,7 @@ const Healthcare = () => {
         </section>
       </div>
       <HealthcareModal show={isModalOpen} handleClose={handleCloseModal} selectedCategory={selectedCategory} />
+      <HealthcareModal2 show={isModalOpen2} handleClose={handleCloseModal2} selectedCategory={selectedCategory} />
     </div>
   );
 }
