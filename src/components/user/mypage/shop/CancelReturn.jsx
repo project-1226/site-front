adf6e374bf2c67df491ba7a2388738298e376c63
Seonu1015 <ulxovoxlu1015@gmail.com>
2024-01-16 23:05:30 +0900

    Feat: 반품 내역 출력(주문상세페이지 연동 필요)

--- src/components/user/mypage/shop/CancelReturn.jsx
+++ src/components/user/mypage/shop/CancelReturn.jsx
@@ -26,6 +26,21 @@ const CancelReturn = () => {
     setPage(value);
   };
 
+  const getList = async () => {
+    setLoading(true);
+    const res = await axios("/usershop/return/list", {
+      params: { userid: sessionStorage.getItem("userid") },
+    });
+    setTotal(res.data.total);
+    console.log(res.data.list);
+    setList(res.data.list);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getList();
+  }, []);
+
   return (
     <Box sx={{ width: "100%", bgcolor: "transparent", py: 5, pr: 3 }}>
       <Backdrop
@@ -37,56 +52,60 @@ const CancelReturn = () => {
       <Typography variant="h6" gutterBottom sx={{ fontWeight: "bolder" }}>
         반품내역
       </Typography>
-
-      <Card sx={{ minWidth: 275, mt: 2 }}>
-        <CardContent>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ fontWeight: "bolder", ml: 1, mb: 1 }}
-          >
-            (날짜) 반품
-          </Typography>
-          <Card variant="outlined">
-            <CardContent sx={{ pb: 0 }}>
-              <Typography
-                variant="h6"
-                component="div"
-                sx={{ fontWeight: "bolder" }}
-              >
-                반품진행상황
-              </Typography>
-            </CardContent>
-            <Stack direction="row" mx={3} mb={1}>
-              <CardContent sx={{ flex: 1, alignSelf: "center" }}>
-                <Stack spacing={3}>
-                  <Stack direction="row" spacing={3}>
-                    <CardMedia
-                      component="img"
-                      alt="product"
-                      sx={{ width: 82 }}
-                    />
-                    <Stack spacing={1}>
-                      <Typography color="text">상품명</Typography>
-                      <Typography variant="body2">가격 / 수량</Typography>
+      {list.map((l) => (
+        <Card key={l.cancelid} sx={{ minWidth: 275, mt: 2 }}>
+          <CardContent>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ fontWeight: "bolder", ml: 1, mb: 1 }}
+            >
+              {l.fmtdate} 반품
+            </Typography>
+            <Card variant="outlined">
+              <CardContent sx={{ pb: 0 }}>
+                <Typography
+                  variant="body1"
+                  component="div"
+                  sx={{ fontWeight: "bolder" }}
+                >
+                  {l.str_status}
+                </Typography>
+              </CardContent>
+              <Stack direction="row" mx={3} mb={1}>
+                <CardContent sx={{ flex: 1, alignSelf: "center" }}>
+                  <Stack spacing={3}>
+                    <Stack direction="row" spacing={3}>
+                      <CardMedia
+                        component="img"
+                        alt="product"
+                        src={l.image_url}
+                        sx={{ width: 82 }}
+                      />
+                      <Stack spacing={1}>
+                        <Typography color="text">{l.name}</Typography>
+                        <Typography variant="caption" color="text.secondary">
+                          {l.fmtprice}원 / {l.count}개
+                        </Typography>
+                        <Typography variant="body2">
+                          반품 사유 : {l.content}
+                        </Typography>
+                      </Stack>
                     </Stack>
                   </Stack>
-                </Stack>
-              </CardContent>
-              <CardContent sx={{ width: 170, alignSelf: "center", pt: 3 }}>
-                <Stack spacing={1}>
-                  <Button variant="outlined" size="small" fullWidth>
-                    주문 상세 보기
-                  </Button>
-                  <Button variant="outlined" size="small" fullWidth>
-                    반품 상세 보기
-                  </Button>
-                </Stack>
-              </CardContent>
-            </Stack>
-          </Card>
-        </CardContent>
-      </Card>
+                </CardContent>
+                <CardContent sx={{ width: 170, alignSelf: "center", pt: 3 }}>
+                  <Stack spacing={1}>
+                    <Button variant="outlined" size="small" fullWidth>
+                      주문 상세 보기
+                    </Button>
+                  </Stack>
+                </CardContent>
+              </Stack>
+            </Card>
+          </CardContent>
+        </Card>
+      ))}
       <Pagination
         count={Math.ceil(total / size)}
         shape="rounded"
