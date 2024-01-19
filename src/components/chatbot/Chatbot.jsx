755462dcf3de841a0cebd7e29e3d21db9af695cc
kumhee <kumheee95@gmail.com>
2024-01-19 20:22:59 +0900

    FEAT: 챗봇 수정

--- src/components/chatbot/Chatbot.jsx
+++ src/components/chatbot/Chatbot.jsx
@@ -1,49 +1,49 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ChatBot from 'react-simple-chatbot'
-import {ThemeProvider} from 'styled-components'
+import { ThemeProvider } from 'styled-components'
 import "react-chatbot-kit/build/main.css";
 import "../../css/chatbot.css";
 import ChatIcon from '@mui/icons-material/Chat';
 import ContactSupportIcon from '@mui/icons-material/ContactSupport';
 const Chatbot = () => {
-    const [chatbotOpened, setChatbotOpened] = useState(false);
-    const [user, setUser] = useState("");
+  const [chatbotOpened, setChatbotOpened] = useState(false);
+  const [user, setUser] = useState("");
 
 
-    const getUser = async () => {
-      const res = await axios("/user/read", {
-        params: {
-          userid: sessionStorage.getItem("userid"),
-        },
-      });
-      setUser(res.data);
-      console.log(user);
-    };
-  
-    const handleChatbotToggle = () => {
-        setChatbotOpened(!chatbotOpened);
-      };
-      const userid = sessionStorage.getItem("userid");
-      
+  const getUser = async () => {
+    const res = await axios("/user/read", {
+      params: {
+        userid: sessionStorage.getItem("userid"),
+      },
+    });
+    setUser(res.data);
+    console.log(user);
+  };
 
-      useEffect(() => {
-        getUser();
-      }, [userid]);
-   
+  const handleChatbotToggle = () => {
+    setChatbotOpened(!chatbotOpened);
+  };
+  const userid = sessionStorage.getItem("userid");
 
 
-    
-    const steps = [
-        {
-            id: '0',
-            message: `${user.nickname}님! 안녕하세요.
-            $어플이름 상담 챗봇입니다.`,
-            trigger: '1',
-        },
-        {
-            id: '1',
-            message: `우리의 서비스가 궁금하셨죠?            
+  useEffect(() => {
+    getUser();
+  }, [userid]);
+
+
+
+
+  const steps = [
+    {
+      id: '0',
+      message: `${user.nickname}님! 안녕하세요.
+            MEALJOY 챗봇 경석방입니다.`,
+      trigger: '1',
+    },
+    {
+      id: '1',
+      message: `우리의 서비스가 궁금하셨죠?            
 
             그럼 소개를 시작해볼까요?
             준비가 되셨다면 시작버튼을
@@ -54,7 +54,7 @@ const Chatbot = () => {
       id: '2',
       options: [
         { value: 1, label: '설명보기', trigger: '3' },
-        { value: 2, label: '시작안하기', trigger: '4' },
+        { value: 2, label: '종료하기', trigger: '4' },
       ],
     },
     {
@@ -64,13 +64,13 @@ const Chatbot = () => {
     },
     {
       id: '4',
-      message: `ㄲㅈ `,
+      message: `ㅇㅉㅌㅂ `,
     },
     {
       id: '5',
       options: [
-        { value: 1, label: '더듣기 ', trigger: '6' },
-        { value: 2, label: '그만보기', trigger: '4' },
+        { value: 1, label: '더 보기 ', trigger: '6' },
+        { value: 2, label: '종료하기', trigger: '4' },
       ],
     },
     {
@@ -80,26 +80,26 @@ const Chatbot = () => {
     },
     {
       id: '7',
-      options: [{ value: 1, label: '끝났습니다 ' }],
+      options: [{ value: 1, label: '상담이 종료되었습니다.' }],
     },
   ];
 
-    const theme = {
-        background: '#f5f8fb',
-        fontFamily: 'Helvetica Neue',
-        headerBgColor: '#748769',
-        headerFontColor: '#fff',
-        headerFontSize: '15px',
-        botBubbleColor: '#748769',
-        botFontColor: '#FFF',
-        userBubbleColor: '#fff',
-        userFontColor: '#4a4a4a',
-        posision: 'relative',
-        bottom: '120px'
-    };
-    const handleCloseChatbot = () => {
-        setChatbotOpened(false);
-      };
+  const theme = {
+    background: '#f5f8fb',
+    fontFamily: 'Helvetica Neue',
+    headerBgColor: '#748769',
+    headerFontColor: '#fff',
+    headerFontSize: '15px',
+    botBubbleColor: '#748769',
+    botFontColor: '#FFF',
+    userBubbleColor: '#fff',
+    userFontColor: '#4a4a4a',
+    posision: 'relative',
+    bottom: '120px'
+  };
+  const handleCloseChatbot = () => {
+    setChatbotOpened(false);
+  };
 
   const ChatbotContent = () => {
     return (
@@ -113,36 +113,29 @@ const Chatbot = () => {
     );
   };
 
-      return (
-        <div className='ak_wrap'>
-          <div className='ak_contents'>
-        
-            <div class='cbfloat-container'>
-            {chatbotOpened &&   
-             <>
-         <div className='leDQrA'>
-         <ThemeProvider theme={theme}>
-                <ChatBot
-                  steps={steps}
-                  hideHeader={true}
-                  placeholder={'채팅이 불가능한  채널입니다.'}
-                />
-              </ThemeProvider>
-         </div>
-             
-    
-            </>}
-                <div class='cbfloat-button-container'>
-                    <button
-                    class='btn-hover color-5'
-                    onClick={handleChatbotToggle}
-             >
-                      <ContactSupportIcon       style={{ fontSize: '3rem' }}/>
-                    </button>
-                
-                </div>
-              
-            </div>
+  return (
+    <div className='ak_wrap'>
+      <div className='ak_contents'>
+        <div class='cbfloat-container'>
+          {chatbotOpened &&
+            <>
+              <div className='leDQrA'>
+                <ThemeProvider theme={theme}>
+                  <ChatBot
+                    steps={steps}
+                    hideHeader={true}
+                    placeholder={'채팅이 불가능한  채널입니다.'}
+                  />
+                </ThemeProvider>
+              </div>
+            </>
+          }
+          <div class='cbfloat-button-container'>
+            <button
+              class='btn-hover color-5'
+              onClick={handleChatbotToggle}>
+              <ContactSupportIcon style={{ fontSize: '3rem' }} />
+            </button>
           </div>
         </div>
       </div>
