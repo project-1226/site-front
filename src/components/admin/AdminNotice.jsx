d9c99a121aadbfd2f6d2461476b362ac3ccdd889
sori-k <vrti0913@naver.com>
2024-01-21 16:15:43 +0900

    Feat: 관리자페이지 후기/댓글 페이지 구현

--- src/components/admin/AdminNotice.jsx
+++ src/components/admin/AdminNotice.jsx
@@ -6,58 +6,58 @@ import { useNavigate } from 'react-router-dom';
 import "../../css/admin.css"
 
 const AdminNotice = () => {
-    const navi = useNavigate();
-    const [loading, setLoading] = useState(false);
+  const navi = useNavigate();
+  const [loading, setLoading] = useState(false);
 
-    const [form, setForm] = useState({
-        categoryid : '101',
-        title: '',
-        userid: '',
-        content: ''
+  const [form, setForm] = useState({
+    categoryid: '101',
+    title: '',
+    userid: '',
+    content: ''
+  });
+
+  const { title, userid, content } = form;
+
+  const onChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
     });
+  }
 
-    const {title, userid, content} = form;
 
-    const onChange = (e) => {
-        setForm({
-            ...form,
-            [e.target.name]:e.target.value
-        });
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (window.confirm("등록할까요?")) {
+      await axios.post('/admin/insert', form);
+      alert("등록 완료!");
+      navi('/community');
     }
+  }
 
+  if (loading) return <div className='my-5'>loading...</div>
 
-    const onSubmit = async(e) => {
-        e.preventDefault();
-        if(window.confirm("등록할까요?")){
-            await axios.post('/admin/insert', form);
-            alert("등록 완료!");
-            navi('/community');
-        }
-    }
-
-    if(loading) return <div className='my-5'>loading...</div>
-
-    return (
-        <div className='notice_register my-5'>
-            <div className='mb-3'>[공지 등록]</div>
-                <form onSubmit={onSubmit} className='mt-4'>
-                    <InputGroup className='mb-2'>
-                        <InputGroup.Text>제목</InputGroup.Text>
-                        <Form.Control name='title' value={title} onChange={onChange}/>
-                    </InputGroup>
-                    <InputGroup className='mb-2'>
-                        <InputGroup.Text>작성자</InputGroup.Text>
-                        <Form.Control name='userid' value={userid} onChange={onChange} />
-                    </InputGroup>
-                    <textarea className='form-control mt-3' name='content' value={content}
-                        onChange={onChange} rows={10} placeholder='내용을 입력하세요.' />
-                    <div className='text-center mt-5'>
-                        <Button type="reset" variant='outlined' color='error'>취소</Button>
-                        <Button type="submit" className='ms-3' variant='contained'>등록</Button>
-                    </div>
-                </form>
+  return (
+    <div className='notice_register my-5'>
+      <div className='mb-3'>[공지 등록]</div>
+      <form onSubmit={onSubmit} className='mt-4'>
+        <InputGroup className='mb-2'>
+          <InputGroup.Text>제목</InputGroup.Text>
+          <Form.Control name='title' value={title} onChange={onChange} />
+        </InputGroup>
+        <InputGroup className='mb-2'>
+          <InputGroup.Text>작성자</InputGroup.Text>
+          <Form.Control name='userid' value={userid} onChange={onChange} />
+        </InputGroup>
+        <textarea className='form-control mt-3' name='content' value={content}
+          onChange={onChange} rows={10} placeholder='내용을 입력하세요.' />
+        <div className='text-center mt-5'>
+          <Button type="reset" variant='outlined' color='error'>취소</Button>
+          <Button type="submit" className='ms-3' variant='contained'>등록</Button>
         </div>
-    )
+      </form>
+    </div>
+  )
 }
 
 export default AdminNotice
--- src/components/admin/ProductSearchPage.jsx
+++ src/components/admin/ProductSearchPage.jsx
@@ -7,155 +7,157 @@ import '../../css/admin.css'
 import "./Pagination.css";
 
 const ProductSearchPage = () => {
-    const [loading, setLoading] = useState(false);
-    const [list, setList] = useState([]);
-    const [page, setPage] = useState(1);
-    const [query, setQuery] = useState("저염식단");
-    const [cnt, setCnt] = useState(0);
-    const [total, setTotal] = useState();
-    const size=5;
-    
+  const [loading, setLoading] = useState(false);
+  const [list, setList] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("저염식단");
+  const [cnt, setCnt] = useState(0);
+  const [total, setTotal] = useState();
+  const size = 5;
 
-    const getList = async() => {
-        setLoading(true);
-        const res = await axios.get(`/admin/product/list?page=${page}&size=5&query=${query}`);
-        console.log(res.data);
 
-        let data = res.data.items.map(p=> p && {...p, name:stripHtmlTags(p.title), price:p.lprice});
-        data= data.map(item=>item && {...item, checked:false});
-        setList(data);
-        setTotal(res.data.total);
-        setLoading(false);
-        //console.log(list);
+  const getList = async () => {
+    setLoading(true);
+    const res = await axios.get(`/admin/product/list?page=${page}&size=5&query=${query}`);
+    //console.log(res.data);
+
+    let data = res.data.items.map(p => p && { ...p, name: stripHtmlTags(p.title), price: p.lprice });
+    data = data.map(item => item && { ...item, checked: false });
+    setList(data);
+    setTotal(res.data.total);
+    setLoading(false);
+    //console.log(list);
+  }
+
+  useEffect(() => {
+    getList();
+  }, [page]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (query === "") {
+      alert("검색어를 입력하세요.");
+    } else {
+      getList();
     }
+  }
 
-    useEffect(() => {
+  const changeGetList = (cpage) => {
+    //console.log(cpage);
+    setPage(cpage);
+  }
+
+  //HTML 태그 제거하는 함수
+  const stripHtmlTags = (htmlString) => {
+    const doc = new DOMParser().parseFromString(htmlString, 'text/html');
+    return doc.body.textContent || "";
+  }
+
+  const onSave = async (name, price, image, productid) => {
+    if (window.confirm("상품을 등록할까요?")) {
+      await axios.post("/admin/insert/product", { name, price, image_url: image, productid });
+      alert("등록완료!");
+    }
+  }
+
+  const onChangeSingle = (e, productId) => {
+    const data = list.map(item => item.productId === productId ? { ...item, checked: e.target.checked } : item);
+    setList(data);
+  }
+
+  const onChangeAll = (e) => {
+    const data = list.map(item => item && { ...item, checked: e.target.checked });
+    setList(data);
+  }
+
+  const onCheckedSave = async () => {
+    if (cnt === 0) {
+      alert("등록할 상품을 선택하세요!");
+    } else {
+      if (window.confirm(`${cnt}개 상품을 등록할까요?`)) {
+        for (const item of list) {
+          if (item.checked) {
+            await axios.post("/admin/insert/product", {
+              name: item.name,
+              price: item.price,
+              image_url: item.image,
+              productid: item.productid
+            });
+          }
+        }
+        alert("상품등록 완료!");
         getList();
-    }, [page]);
-
-    const onSubmit = (e) => {
-        e.preventDefault();
-        if(query === ""){
-            alert("검색어를 입력하세요.");
-        }else{
-            getList();
-        }
+      }
     }
+  }
 
-    const changeGetList = (cpage) => {
-        //console.log(cpage);
-        setPage(cpage);
-    }
+  useEffect(() => {
+    let chk = 0;
+    list.forEach(item => {
+      if (item.checked) chk++;
+    });
+    setCnt(chk);
+  }, [list]);
 
-    //HTML 태그 제거하는 함수
-    const stripHtmlTags = (htmlString) => {
-        const doc = new DOMParser().parseFromString(htmlString, 'text/html');
-        return doc.body.textContent || "";
-    }
 
-    const onSave = async(name, price, image, productid) => {
-        if(window.confirm("상품을 등록할까요?")){
-            await axios.post("/admin/insert/product", {name, price, image_url:image, productid});
-            alert("등록완료!");
-        }
-    }
+  if (loading) return <div className='text-center my-5'><Spinner /></div>
 
-    const onChangeSingle = (e, productId) => {
-        const data = list.map(item=> item.productId === productId ? {...item, checked:e.target.checked} : item);
-        setList(data);
-    }
-
-    const onChangeAll = (e) => {
-        const data = list.map(item=> item && {...item, checked:e.target.checked});
-        setList(data);
-    }
-
-    const onCheckedSave = async() => {
-        if(cnt === 0){
-            alert("등록할 상품을 선택하세요!");
-        }else{
-            if(window.confirm(`${cnt}개 상품을 등록할까요?`)){
-                for(const item of list){
-                    if(item.checked){
-                        await axios.post("/admin/insert/product", {
-                            name: item.name,
-                            price: item.price,
-                            image_url: item.image,
-                            productid: item.productid
-                        });
-                    }
+  return (
+    <div className='my-3'>
+      <h5 className='mb-3'>상품검색</h5>
+      <Col md={4} className='mt-5'>
+        <form onSubmit={onSubmit}>
+          <InputGroup>
+            <Form.Control onChange={(e) => setQuery(e.target.value)} placeholder='상품명' value={query} />
+            <Button type='submit' variant='outlined' size='small'>검색</Button>
+          </InputGroup>
+        </form>
+      </Col>
+      <div className='text-end mx-5' style={{ paddingRight: '8%' }}>
+        <Button onClick={onCheckedSave} variant='outlined' size='small'>선택저장</Button>
+      </div>
+      <Table className='mt-3' style={{ width: '1100px' }}>
+        <thead>
+          <tr>
+            <th className='text-center'><input type='checkbox' onChange={onChangeAll} checked={list.length === cnt} /></th>
+            <th style={{ width: '130px' }}>Image</th>
+            <th className='text-center' style={{ width: '480px' }}>title</th>
+            <th className='text-center'>가격</th>
+          </tr>
+        </thead>
+        <tbody>
+          {list.map(p =>
+            <tr key={p.productId}>
+              <td className='text-center'><input type='checkbox' onChange={(e) => onChangeSingle(e, p.productId)}
+                checked={p.checked} /></td>
+              <td>
+                {p.image ?
+                  <img src={p.image} width='50px' height='50px' />
+                  :
+                  <img src='http://via.placeholder.com/50x50' />
                 }
-                alert("상품등록 완료!");
-                getList();
-            }
-        }
-    }
-
-    useEffect(() => {
-        let chk = 0;
-        list.forEach(item=> {
-            if(item.checked) chk++;
-        });
-        setCnt(chk);
-    }, [list]);
-
-
-    if(loading) return <div className='text-center my-5'><Spinner/></div>
-
-    return (
-        <div className='my-3'>
-            <h5 className='mb-3'>상품검색</h5>
-            <Col md={4} className='mt-5'>
-                <form onSubmit={onSubmit}>
-                    <InputGroup>
-                        <Form.Control onChange={(e)=> setQuery(e.target.value)} placeholder='상품명' value={query} />
-                        <Button type='submit' variant='outlined' size='small'>검색</Button>
-                    </InputGroup>
-                </form>
-            </Col>
-            <div className='text-end mx-5' style={{paddingRight: '8%'}}>
-                <Button onClick={onCheckedSave} variant='outlined' size='small'>선택저장</Button>
-            </div>
-            <Table className='mt-3' style={{width:'1100px'}}>
-                <thead>
-                    <tr>
-                        <th className='text-center'><input type='checkbox' onChange={onChangeAll} checked={list.length === cnt} /></th>
-                        <th style={{width:'130px'}}>Image</th>
-                        <th className='text-center' style={{width:'480px'}}>title</th>
-                        <th className='text-center'>가격</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {list.map(p=>
-                        <tr key={p.productId}>
-                            <td className='text-center'><input type='checkbox' onChange={(e)=> onChangeSingle(e, p.productId)}
-                                checked={p.checked} /></td>
-                            <td>
-                                {p.image ?
-                                    <img src={p.image} width='50px' height='50px' />
-                                    :
-                                    <img src='http://via.placeholder.com/50x50' />
-                                }
-                            </td>
-                            <td ><div className='ellipsis_adpro'>{p.name}</div></td>
-                            <td className='text-center'>{p.lprice}원</td>
-                            <div><Button onClick={()=> onSave(p.name, p.price, p.image, p.productid)} variant='contained' size='small'>등록</Button></div>
-                        </tr>
-                    )}
-                </tbody>
-            </Table>
-                {total > size &&
-                    <Pagination
-                        activePage={page}
-                        itemsCountPerPage={size}
-                        totalItemsCount={total}
-                        pageRangeDisplayed={10}
-                        prevPageText={"‹"}
-                        nextPageText={"›"}
-                        onChange={(cpage)=>{changeGetList(cpage)}}/>
-                }
-        </div>
-    )
+              </td>
+              <td ><div className='ellipsis_adpro'>{p.name}</div></td>
+              <td className='text-center'>{p.lprice}원</td>
+              <div>
+                <Button onClick={() => onSave(p.name, p.price, p.image, p.productid)} variant='contained' size='small'>등록</Button>
+              </div>
+            </tr>
+          )}
+        </tbody>
+      </Table>
+      {total > size &&
+        <Pagination
+          activePage={page}
+          itemsCountPerPage={size}
+          totalItemsCount={total}
+          pageRangeDisplayed={10}
+          prevPageText={"‹"}
+          nextPageText={"›"}
+          onChange={(cpage) => { changeGetList(cpage) }} />
+      }
+    </div>
+  )
 }
 
 export default ProductSearchPage
