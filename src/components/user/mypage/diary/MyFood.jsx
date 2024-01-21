a2c8856dd09d40a026dfd131249bfa152ef53c3c
Seonu1015 <ulxovoxlu1015@gmail.com>
2024-01-21 12:16:38 +0900

    Merge remote-tracking branch 'origin/develop' into userFeature

--- src/components/user/mypage/diary/MyFood.jsx
+++ src/components/user/mypage/diary/MyFood.jsx
@@ -17,7 +17,6 @@ import DietModal from "../../../myDiet/DietModal";
 const MyFood = ({ userInfo }) => {
   const [loading, setLoading] = useState(false);
   const [list, setList] = useState([]);
-  const [isFoodChanged, setIsFoodChanged] = useState(false);
 
   const getPlan = async (regdate) => {
     const res = await axios("/food/mypage/plan", {
